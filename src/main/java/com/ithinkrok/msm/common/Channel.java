b15af76bac484efd91d1c9213b7734a17c422d82
paul <pauldurbz@gmail.com>
2016-02-04 19:12:24 +0000

    Rename MSMChannel to Channel

--- src/main/java/com/ithinkrok/msm/common/Channel.java
+++ src/main/java/com/ithinkrok/msm/common/Channel.java
@@ -5,7 +5,7 @@ import org.bukkit.configuration.ConfigurationSection;
 /**
  * Created by paul on 03/02/16.
  */
-public interface MSMChannel {
+public interface Channel {
 
     void write(ConfigurationSection packet);
 }
