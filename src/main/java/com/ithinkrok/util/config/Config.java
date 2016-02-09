a97e53ea31823eb5036b41264cebe75b0e181d7b
paul <pauldurbz@gmail.com>
2016-02-09 19:34:27 +0000

    Add method getType to Config

--- src/main/java/com/ithinkrok/util/config/Config.java
+++ src/main/java/com/ithinkrok/util/config/Config.java
@@ -7,133 +7,141 @@ import java.util.*;
  */
 public interface Config {
 
-    Set<String> getKeys(boolean deep);
-
     Map<String, Object> getValues(boolean deep);
 
     boolean contains(String path);
 
-    Object get(String path, Object def);
-
-    default Object get(String path){
-        return get(path, null);
+    default void setAll(Map<String, Object> values) {
+        for (String path : values.keySet()) {
+            set(path, values.get(path));
+        }
     }
 
     void set(String path, Object value);
 
-    default void setAll(Map<String, Object> values){
-        for(String path : values.keySet()) {
-            set(path, values.get(path));
-        }
-    }
-
     default void setAll(Config values) {
-        for(String path : values.getKeys(true)) {
+        for (String path : values.getKeys(true)) {
             set(path, values.get(path));
         }
     }
 
-    default String getString(String path, String def){
-        Object obj = get(path);
+    Set<String> getKeys(boolean deep);
 
-        if(obj != null) return obj.toString();
-        else return def;
+    default Object get(String path) {
+        return get(path, null);
     }
+
+    Object get(String path, Object def);
 
     default String getString(String path) {
         return getString(path, null);
     }
 
-    default boolean isString(String path){
-        return get(path) instanceof String;
-    }
-
-    default int getInt(String path, int def){
+    default String getString(String path, String def) {
         Object obj = get(path);
 
-        if(obj instanceof Number) return ((Number) obj).intValue();
+        if (obj != null) return obj.toString();
         else return def;
+    }
+
+    default boolean isString(String path) {
+        return get(path) instanceof String;
     }
 
     default int getInt(String path) {
         return getInt(path, 0);
     }
 
-    default boolean isInt(String path){
-        return get(path) instanceof Integer;
-    }
-
-    default boolean getBoolean(String path, boolean def){
+    default int getInt(String path, int def) {
         Object obj = get(path);
 
-        if(obj instanceof Boolean) return (Boolean) obj;
+        if (obj instanceof Number) return ((Number) obj).intValue();
         else return def;
+    }
+
+    default boolean isInt(String path) {
+        return get(path) instanceof Integer;
     }
 
     default boolean getBoolean(String path) {
         return getBoolean(path, false);
     }
 
-    default boolean isBoolean(String path){
-        return get(path) instanceof Boolean;
-    }
-
-    default double getDouble(String path, double def){
+    default boolean getBoolean(String path, boolean def) {
         Object obj = get(path);
 
-        if(obj instanceof Number) return ((Number) obj).doubleValue();
+        if (obj instanceof Boolean) return (Boolean) obj;
         else return def;
+    }
+
+    default boolean isBoolean(String path) {
+        return get(path) instanceof Boolean;
     }
 
     default double getDouble(String path) {
         return getDouble(path, 0.0d);
     }
 
-    default boolean isDouble(String path){
-        return get(path) instanceof Boolean;
-    }
-
-    default long getLong(String path, long def){
+    default double getDouble(String path, double def) {
         Object obj = get(path);
 
-        if(obj instanceof Number) return ((Number) obj).longValue();
+        if (obj instanceof Number) return ((Number) obj).doubleValue();
         else return def;
+    }
+
+    default boolean isDouble(String path) {
+        return get(path) instanceof Boolean;
     }
 
     default long getLong(String path) {
         return getLong(path, 0L);
     }
 
-    default boolean isLong(String path){
-        return get(path) instanceof Long;
-    }
-
-    default byte[] getByteArray(String path, byte[] def){
+    default long getLong(String path, long def) {
         Object obj = get(path);
 
-        if(obj instanceof byte[]) return (byte[]) obj;
+        if (obj instanceof Number) return ((Number) obj).longValue();
         else return def;
+    }
+
+    default boolean isLong(String path) {
+        return get(path) instanceof Long;
     }
 
     default byte[] getByteArray(String path) {
         return getByteArray(path, new byte[0]);
     }
 
-    default boolean isByteArray(String path){
+    default byte[] getByteArray(String path, byte[] def) {
+        Object obj = get(path);
+
+        if (obj instanceof byte[]) return (byte[]) obj;
+        else return def;
+    }
+
+    default boolean isByteArray(String path) {
         return get(path) instanceof byte[];
     }
 
+    default List<String> getStringList(String path) {
+        return getList(path, String.class);
+    }
+
+    default <T> List<T> getList(String path, Class<T> clazz) {
+        return getList(path, new ArrayList<>(), clazz);
+    }
+
     @SuppressWarnings("unchecked")
-    default <T> List<T> getList(String path, List<T> def, Class<T> clazz){
+    default <T> List<T> getList(String path, List<T> def, Class<T> clazz) {
         Object obj = get(path);
 
-        if(!(obj instanceof Collection<?>)) return def;
+        if (!(obj instanceof Collection<?>)) return def;
 
         Collection<?> input = (Collection<?>) obj;
         List<T> result = new ArrayList<>();
 
-        for(Object o : input) {
-            if(!clazz.isInstance(o)) continue;
+        for (Object o : input) {
+            if (!clazz.isInstance(o)) continue;
 
             result.add((T) o);
         }
@@ -141,44 +149,47 @@ public interface Config {
         return result;
     }
 
-    default <T> List<T> getList(String path, Class<T> clazz) {
-        return getList(path, new ArrayList<>(), clazz);
-    }
-
-    default List<String> getStringList(String path) {
-        return getList(path, String.class);
-    }
-
     default List<Config> getConfigList(String path) {
         return getList(path, Config.class);
     }
 
-    default boolean isList(String path){
+    default boolean isList(String path) {
         return get(path) instanceof List<?>;
     }
 
     /**
-     *
      * @param path The path to check for a list
      * @param type The type that the list should contain
      * @return If there is a list at this path and it contains an object with the type {@code type}
      */
-    default boolean isList(String path, Class<?> type){
-        if(!isList(path)) return false;
+    default boolean isList(String path, Class<?> type) {
+        if (!isList(path)) return false;
 
         List<?> list = getList(path, null, type);
 
         return list != null && !list.isEmpty();
     }
 
-    default Config getConfig(String path){
+    default Config getConfig(String path) {
         Object obj = get(path);
 
-        if(!(obj instanceof Config)) return null;
+        if (!(obj instanceof Config)) return null;
         else return (Config) obj;
     }
 
-    default boolean isConfig(String path){
+    default boolean isConfig(String path) {
         return get(path) instanceof Config;
+    }
+
+    default <T> T getType(String path, Class<T> type) {
+        return getType(path, null, type);
+    }
+
+    @SuppressWarnings("unchecked")
+    default <T> T getType(String path, T def, Class<T> type) {
+        Object obj = get(path);
+
+        if (!type.isInstance(obj)) return def;
+        return (T) obj;
     }
 }
