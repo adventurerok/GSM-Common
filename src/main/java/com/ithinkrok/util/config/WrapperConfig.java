490a4dcd2020fac53aa9e72245b7e86838323cc6
paul <pauldurbz@gmail.com>
2016-02-10 20:14:37 +0000

    Created WrapperConfig to wrap around another config

---
+++ src/main/java/com/ithinkrok/util/config/WrapperConfig.java
@@ -0,0 +1,58 @@
+package com.ithinkrok.util.config;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by paul on 10/02/16.
+ *
+ * Intended to be overridden to provide extra config functionality
+ */
+public class WrapperConfig implements Config {
+
+    protected final Config wrappedConfig;
+
+    public WrapperConfig(Config wrappedConfig) {
+
+        //Ensure we do not have multiple nested WrapperConfigs
+        if(wrappedConfig instanceof WrapperConfig) {
+            this.wrappedConfig = ((WrapperConfig) wrappedConfig).getWrappedConfig();
+        } else {
+            this.wrappedConfig = wrappedConfig;
+        }
+    }
+
+    public Config getWrappedConfig() {
+        return wrappedConfig;
+    }
+
+    @Override
+    public Map<String, Object> getValues(boolean deep) {
+        return wrappedConfig.getValues(deep);
+    }
+
+    @Override
+    public boolean contains(String path) {
+        return wrappedConfig.contains(path);
+    }
+
+    @Override
+    public void set(String path, Object value) {
+        wrappedConfig.set(path, value);
+    }
+
+    @Override
+    public Set<String> getKeys(boolean deep) {
+        return wrappedConfig.getKeys(deep);
+    }
+
+    @Override
+    public Object get(String path, Object def) {
+        return wrappedConfig.get(path, def);
+    }
+
+    @Override
+    public Config getConfigOrEmpty(String path) {
+        return wrappedConfig.getConfigOrEmpty(path);
+    }
+}
