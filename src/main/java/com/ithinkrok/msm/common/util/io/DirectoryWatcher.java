1fbccfbc69c5864aa4934df1ad20684dccee5b19
paul <pauldurbz@gmail.com>
2016-02-16 17:55:06 +0000

    Fix alerting DirectoryListeners multiple times of a change

--- src/main/java/com/ithinkrok/msm/common/util/io/DirectoryWatcher.java
+++ src/main/java/com/ithinkrok/msm/common/util/io/DirectoryWatcher.java
@@ -131,7 +131,7 @@ public class DirectoryWatcher {
 
                     for (PathListenerGroup pathListenerGroup : targets) {
                         //Made sure the listeners in this group are listening for this directory
-                        if (!changed.startsWith(pathListenerGroup.path)) continue;
+                        if (!changed.equals(pathListenerGroup.path)) continue;
 
                         for (DirectoryListener listener : pathListenerGroup.listeners) {
                             try {
