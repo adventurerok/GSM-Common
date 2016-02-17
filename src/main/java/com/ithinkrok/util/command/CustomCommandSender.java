f83c5f89ff50143af88d27a678643e0280ac65b0
paul <pauldurbz@gmail.com>
2016-02-17 17:07:27 +0000

    The server now uses LanguageLookups

--- src/main/java/com/ithinkrok/util/command/CustomCommandSender.java
+++ src/main/java/com/ithinkrok/util/command/CustomCommandSender.java
@@ -1,9 +1,10 @@
 package com.ithinkrok.util.command;
 
+import com.ithinkrok.util.lang.Messagable;
+
 /**
  * Created by paul on 07/02/16.
  */
-public interface CustomCommandSender {
+public interface CustomCommandSender extends Messagable {
 
-    void sendMessage(String message);
 }
