d7bcf67900b13a1abba7dbd7cba963475f147560
paul <pauldurbz@gmail.com>
2016-12-17 14:32:11 +0000

    Allow sending raw messages by using configs

--- src/main/java/com/ithinkrok/util/lang/Messagable.java
+++ src/main/java/com/ithinkrok/util/lang/Messagable.java
@@ -1,15 +1,30 @@
 package com.ithinkrok.util.lang;
 
+import com.ithinkrok.util.config.Config;
+
 /**
  * Created by paul on 02/01/16.
  */
 public interface Messagable {
 
     default void sendMessage(String message){
-        sendMessageNoPrefix(message);
+        sendMessageNoPrefix(getMessagePrefix() + message);
     }
 
     void sendMessageNoPrefix(String message);
+
+    void sendMessageNoPrefix(Config message);
+
+    default void sendMessageNoPrefix(Object message){
+        if(message instanceof Config) {
+            sendMessageNoPrefix((Config)message);
+        } else {
+            if(message == null) sendMessageNoPrefix((String)null);
+            else sendMessageNoPrefix(message.toString());
+        }
+    }
+
+    String getMessagePrefix();
 
     default void sendLocale(String locale, Object... args){
         sendLocaleNoPrefix(locale, args);
