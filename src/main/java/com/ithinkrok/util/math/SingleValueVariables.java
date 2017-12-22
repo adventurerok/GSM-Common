1684c0407b6808086e27267f288bf4545427deff
paul <pauldurbz@gmail.com>
2017-12-22 22:10:08 +0000

    Support for BigDecimal in SingleValueVariables

--- src/main/java/com/ithinkrok/util/math/SingleValueVariables.java
+++ src/main/java/com/ithinkrok/util/math/SingleValueVariables.java
@@ -1,5 +1,7 @@
 package com.ithinkrok.util.math;
 
+import java.math.BigDecimal;
+
 /**
  * Created by paul on 05/01/16.
  */
@@ -7,12 +9,32 @@ public class SingleValueVariables implements Variables {
 
     private final double value;
 
+    private BigDecimal decimal;
+
     public SingleValueVariables(double value) {
         this.value = value;
+    }
+
+    public SingleValueVariables(BigDecimal decimal) {
+        if(decimal == null) {
+            throw new NullPointerException("decimal cannot be null");
+        }
+
+        this.decimal = decimal;
+        this.value = decimal.doubleValue();
     }
 
     @Override
     public double getVariable(String name) {
         return value;
+    }
+
+    @Override
+    public BigDecimal getDecimalVariable(String name) {
+        if(decimal == null) {
+            decimal = BigDecimal.valueOf(value);
+        }
+
+        return decimal;
     }
 }
