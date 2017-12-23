8c180e9d99092177ab14b58b82b0108d38f23d15
paul <pauldurbz@gmail.com>
2017-12-23 02:35:21 +0000

    Add methods to lookup currencies and get the parent context

--- src/main/java/com/ithinkrok/msm/common/economy/CurrencyContext.java
+++ src/main/java/com/ithinkrok/msm/common/economy/CurrencyContext.java
@@ -1,10 +1,36 @@
 package com.ithinkrok.msm.common.economy;
 
+import java.util.Collection;
+
 public interface CurrencyContext {
 
     /**
-     * @return The type of currency that may reside in this context
+     * @return The types of currency that may reside in this context
      */
-    String getCurrencyType();
+    Collection<String> getCurrencyTypes();
+
+
+    /**
+     * @return The currency type specific to this context (and not its parents)
+     */
+    String getContextType();
+
+
+    /**
+     * Looks up a currency in this context, or the parent contexts.
+     *
+     * @param name The name of the currency to lookup
+     * @return The {@link Currency}, if one is found, or else null
+     */
+    Currency lookupCurrency(String name);
+
+
+    /**
+     * Gets the parent currency context to this one,
+     * or null if this is the highest currency context existing (the global context usually)
+     *
+     * @return The parent context to this one
+     */
+    CurrencyContext getParent();
 
 }
