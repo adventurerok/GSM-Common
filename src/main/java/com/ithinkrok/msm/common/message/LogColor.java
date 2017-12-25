65be3b16a624198cf6b1c7d41e1db3b7f38544a0
paul <pauldurbz@gmail.com>
2017-12-25 21:43:43 +0000

    More log colors

--- src/main/java/com/ithinkrok/msm/common/message/LogColor.java
+++ src/main/java/com/ithinkrok/msm/common/message/LogColor.java
@@ -4,13 +4,27 @@ public final class LogColor {
 
     public static final String RESET_COLOR = "§r";
 
+    public static final String ARG1_COLOR = "§1";
+    public static final String ARG2_COLOR = "§2";
+    public static final String ARG3_COLOR = "§3";
+
+    public static final String GAME_COLOR = "§5"; //dark purple
+    public static final String OTHER1_COLOR = "§7"; //gray
+    public static final String OTHER2_COLOR = "§8"; //dark gray
     public static final String USER_COLOR = "§a"; //green
     public static final String SERVER_COLOR = "§b"; //aqua
-    public static final String GAME_COLOR = "§5"; //dark purple
     public static final String TYPE_COLOR = "§d"; //pink
 
     public static final String KEY_COLOR = "§b"; //aqua
     public static final String VALUE_COLOR = "§6"; //gold
+
+    public static String FORMAT(String color, Object name) {
+        return FORMAT(color, name, RESET_COLOR);
+    }
+
+    public static String FORMAT(String color, Object name, String reset) {
+        return color + name + reset;
+    }
 
     public static String FORMAT_USER(String name) {
         return FORMAT_USER(name, RESET_COLOR);
