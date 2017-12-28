bfc11da207880962b7bc7a02ccd9d15d07ce6ec8
paul <pauldurbz@gmail.com>
2017-12-28 02:38:07 +0000

    Support for GetBalance requests

--- src/main/java/com/ithinkrok/msm/common/economy/result/Balance.java
+++ src/main/java/com/ithinkrok/msm/common/economy/result/Balance.java
@@ -1,6 +1,10 @@
 package com.ithinkrok.msm.common.economy.result;
 
 import com.ithinkrok.msm.common.economy.Currency;
+import com.ithinkrok.util.config.Config;
+import com.ithinkrok.util.config.ConfigDeserializer;
+import com.ithinkrok.util.config.ConfigSerializer;
+import com.ithinkrok.util.config.MemoryConfig;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -27,5 +31,24 @@ public class Balance {
 
     public BigDecimal getAmount() {
         return amount;
+    }
+
+    public Config toConfig(ConfigSerializer<Currency> currencySerializer) {
+        Config config = new MemoryConfig();
+
+        config.set("account", account.toString());
+        config.set("currency", currencySerializer.serialize(currency));
+        config.set("amount", amount);
+
+        return config;
+    }
+
+    public static Balance fromConfig(Config config, ConfigDeserializer<Currency> currencyDeserializer) {
+
+        UUID account = UUID.fromString(config.getString("account"));
+        Currency currency = currencyDeserializer.deserialize(config.getConfigOrNull("currency"));
+        BigDecimal amount = config.getBigDecimal("amount");
+
+        return new Balance(account, currency, amount);
     }
 }
