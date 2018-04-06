5197a8e55aa3e9e1e986529082d22876e01cc99c
paul <pauldurbz@gmail.com>
2018-04-06 16:31:45 +0100

    Create MutableVariables

---
+++ src/main/java/com/ithinkrok/util/math/MutableVariables.java
@@ -0,0 +1,26 @@
+package com.ithinkrok.util.math;
+
+import java.math.BigDecimal;
+
+public interface MutableVariables extends Variables {
+
+    /**
+     * Sets the value of the variable specified by name
+     *
+     * @param name The name of the variable to set
+     * @param value The value to set the variable to
+     */
+    void setVariable(String name, double value);
+
+    /**
+     * Sets the value of the variable specified by name. By default converts the BigDecimal to a double and calls
+     * {@link #setVariable(String, double)}
+     *
+     * @param name The name of the variable to set
+     * @param value The value to set the variable to
+     */
+    default void setVariable(String name, BigDecimal value) {
+        setVariable(name, value.doubleValue());
+    }
+
+}
