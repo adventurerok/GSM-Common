193d9c5493cb2fdd16d4857ccfd4a316c03f20f0
paul <pauldurbz@gmail.com>
2018-09-09 13:56:04 +0100

    Support for executing batches across multiple providers

--- src/main/java/com/ithinkrok/msm/common/economy/provider/LookupEconomyProvider.java
+++ src/main/java/com/ithinkrok/msm/common/economy/provider/LookupEconomyProvider.java
@@ -7,12 +7,29 @@ import com.ithinkrok.msm.common.economy.batch.BatchResult;
 import com.ithinkrok.msm.common.economy.batch.Update;
 import com.ithinkrok.msm.common.economy.batch.UpdateResult;
 import com.ithinkrok.msm.common.economy.result.Balance;
+import com.ithinkrok.msm.common.economy.result.BalanceChange;
 import com.ithinkrok.msm.common.economy.result.MultiBalanceResult;
+import com.ithinkrok.msm.common.economy.result.TransactionResult;
+import com.ithinkrok.util.NullReplacements;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public abstract class LookupEconomyProvider implements EconomyProvider {
+
+
+    @Override
+    public boolean hasLocalAccount(AccountIdentifier account) {
+        return lookupProviderForAccount(account).hasLocalAccount(account);
+    }
+
+
+    private EconomyProvider lookupProviderForAccount(AccountIdentifier account) {
+        return lookupProviderForCurrency(account.getCurrency());
+    }
 
 
     /**
@@ -24,29 +41,28 @@ public abstract class LookupEconomyProvider implements EconomyProvider {
 
 
     @Override
-    public boolean hasLocalAccount(AccountIdentifier account) {
-        return lookupProviderForAccount(account).hasLocalAccount(account);
-    }
-
-    @Override
     public void hasAccount(AccountIdentifier account, Consumer<Boolean> consumer) {
         lookupProviderForAccount(account).hasAccount(account, consumer);
     }
+
 
     @Override
     public Optional<Boolean> hasAccount(AccountIdentifier account) {
         return lookupProviderForAccount(account).hasAccount(account);
     }
 
+
     @Override
     public void getBalance(AccountIdentifier account, Consumer<Balance> consumer) {
         lookupProviderForAccount(account).getBalance(account, consumer);
     }
 
+
     @Override
     public Optional<Balance> getBalance(AccountIdentifier account) {
         return lookupProviderForAccount(account).getBalance(account);
     }
+
 
     @Override
     public void getBalances(Set<UUID> uuids, Set<Currency> allCurrencies, Consumer<MultiBalanceResult> consumer) {
@@ -76,40 +92,51 @@ public abstract class LookupEconomyProvider implements EconomyProvider {
 
     @Override
     public void executeBatch(Batch batch, String reason, Consumer<BatchResult> consumer) {
-        if(batch.getUpdates().isEmpty()) {
+        if (batch.getUpdates().isEmpty()) {
             consumer.accept(new BatchResult(Collections.emptyList(), true));
         }
 
+        if (executeBatchIfSameProvider(batch, reason, consumer)) {
+            return;
+        }
+
+        executeBatchMultipleProviders(batch, reason, consumer);
+    }
+
+
+    void executeBatchMultipleProviders(Batch batch, String reason, Consumer<BatchResult> consumer) {
+        Map<EconomyProvider, Batch> targets = new HashMap<>();
+
+        for (Update update : batch.getUpdates()) {
+            EconomyProvider provider = lookupProviderForAccount(update.getAccount());
+
+            Batch forProvider = targets.computeIfAbsent(provider, economyProvider -> new Batch());
+            forProvider.addUpdate(update);
+        }
+
+        AggregateBatchConsumer aggregateConsumer = new AggregateBatchConsumer(consumer, targets.size());
+
+        //TODO it may be safer to execute local batches first, and only do global ones if local ones are successful
+
+        for (Map.Entry<EconomyProvider, Batch> entry : targets.entrySet()) {
+            entry.getKey().executeBatch(batch, reason, batchResult -> {
+                aggregateConsumer.accept(entry.getKey(), batchResult);
+            });
+        }
+    }
+
+
+    boolean executeBatchIfSameProvider(Batch batch, String reason, Consumer<BatchResult> consumer) {
         EconomyProvider provider = lookupProviderForAccount(batch.getUpdates().get(0).getAccount());
 
         for (Update update : batch.getUpdates()) {
-            if(lookupProviderForAccount(update.getAccount()) != provider) {
-                throw new UnsupportedOperationException("We don't support batch updates across multiple providers yet");
+            if (lookupProviderForAccount(update.getAccount()) != provider) {
+                return false;
             }
         }
 
         provider.executeBatch(batch, reason, consumer);
-
-        //TODO support for cross provider batches
-//        Map<EconomyProvider, Batch> targets = new HashMap<>();
-//
-//        for (Update update : batch.getUpdates()) {
-//            EconomyProvider provider = lookupProviderForAccount(update.getAccount());
-//
-//            Batch forProvider = targets.computeIfAbsent(provider, economyProvider -> new Batch());
-//            forProvider.addUpdate(update);
-//        }
-//
-//        Consumer<BatchResult> aggregateConsumer = new AggregateBatchConsumer(consumer, targets.size());
-//
-//        for (Map.Entry<EconomyProvider, Batch> entry : targets.entrySet()) {
-//            entry.getKey().executeBatch(batch, reason, aggregateConsumer);
-//        }
-    }
-
-
-    private EconomyProvider lookupProviderForAccount(AccountIdentifier account) {
-        return lookupProviderForCurrency(account.getCurrency());
+        return true;
     }
 
 
@@ -120,6 +147,7 @@ public abstract class LookupEconomyProvider implements EconomyProvider {
 
         int lastResultsReceived;
 
+
         private AggregateMultiBalanceConsumer(Consumer<MultiBalanceResult> target, int lastResultsExpected) {
             this.target = target;
             this.lastResultsExpected = lastResultsExpected;
@@ -128,16 +156,16 @@ public abstract class LookupEconomyProvider implements EconomyProvider {
 
         @Override
         public void accept(MultiBalanceResult multiBalanceResult) {
-            if(!multiBalanceResult.isLastResult()) {
+            if (!multiBalanceResult.isLastResult()) {
                 target.accept(multiBalanceResult);
                 return;
             }
 
-            if(multiBalanceResult.isLastResult()) {
+            if (multiBalanceResult.isLastResult()) {
                 ++lastResultsReceived;
             }
 
-            if(lastResultsReceived == lastResultsExpected) {
+            if (lastResultsReceived == lastResultsExpected) {
                 target.accept(multiBalanceResult);
             } else {
                 target.accept(new MultiBalanceResult(multiBalanceResult.getBalances(), false));
@@ -146,25 +174,87 @@ public abstract class LookupEconomyProvider implements EconomyProvider {
     }
 
     // TODO implement support for cross provider Batches
-//    private class AggregateBatchConsumer implements Consumer<BatchResult> {
-//
-//        final Consumer<BatchResult> target;
-//        final int resultsExpected;
-//
-//        Collection<BatchResult> successfulResults = new ArrayList<>();
-//
-//        boolean failure;
-//        int resultsRecieved;
-//
-//        public AggregateBatchConsumer(Consumer<BatchResult> target, int resultsExpected) {
-//            this.target = target;
-//            this.resultsExpected = resultsExpected;
-//        }
-//
-//
-//        @Override
-//        public void accept(BatchResult batchResult) {
-//
-//        }
-//    }
+    private class AggregateBatchConsumer {
+
+        final Consumer<BatchResult> target;
+        final int resultsExpected;
+
+        Map<EconomyProvider, BatchResult> successfulResults = new ConcurrentHashMap<>();
+        Map<EconomyProvider, BatchResult> failedResults = new ConcurrentHashMap<>();
+
+        AtomicBoolean sentResult = new AtomicBoolean(false);
+
+
+        public AggregateBatchConsumer(Consumer<BatchResult> target, int resultsExpected) {
+            this.target = target;
+            this.resultsExpected = resultsExpected;
+        }
+
+
+        public void accept(EconomyProvider provider, BatchResult batchResult) {
+            if (batchResult.wasSuccessful()) {
+                successfulResults.put(provider, batchResult);
+                if (!failedResults.isEmpty()) {
+                    rollback();
+                }
+            } else {
+                if (failedResults.isEmpty()) {
+                    rollback();
+                } else {
+                    failedResults.put(provider, batchResult);
+                }
+            }
+
+            if((successfulResults.size() == resultsExpected) && sentResult.compareAndSet(false, true)) {
+                List<UpdateResult> results = successfulResults.values().stream()
+                        .map(BatchResult::getResults)
+                        .flatMap(Collection::stream)
+                        .collect(Collectors.toList());
+
+                target.accept(new BatchResult(results, true));
+            }
+
+            if((failedResults.size() == resultsExpected) && sentResult.compareAndSet(false, true)) {
+                List<UpdateResult> results = failedResults.values().stream()
+                        .map(BatchResult::getResults)
+                        .flatMap(Collection::stream)
+                        .collect(Collectors.toList());
+
+                target.accept(new BatchResult(results, false));
+            }
+        }
+
+
+        private void rollback() {
+            for (EconomyProvider provider : successfulResults.keySet()) {
+                successfulResults.computeIfPresent(provider, (economyProvider, batchResult) -> {
+                    Batch rollback = Batch.rollback(batchResult);
+
+                    //this could fail, but then we are truly fucked
+                    provider.executeBatch(rollback, "rollback LookupEconomyProvider",
+                                          NullReplacements.nullConsumer());
+
+
+                    List<UpdateResult> rollbackChanges = batchResult.getResults().stream()
+                            .map(update ->
+                                 {
+                                     BalanceChange noChange = BalanceChange.noBalanceChange(
+                                             update.getChange().getAccount(),
+                                             update.getChange().getOldBalance(),
+                                             "rolled back");
+
+                                     return new UpdateResult(update.getUpdate(),
+                                                             TransactionResult.ROLLED_BACK,
+                                                             noChange);
+                                 })
+                            .collect(Collectors.toList());
+
+                    failedResults.put(provider, new BatchResult(rollbackChanges, false));
+
+                    return null;
+                });
+            }
+
+        }
+    }
 }
